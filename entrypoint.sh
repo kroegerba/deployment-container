e627342cac2351dd3c5da1d18bd118e1f2eda752
Bastian Kröger <bastian.kroeger@pm.me>
2025-11-14 19:50:51 +0100

    add git clone and /destination bind mount

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,25 @@
 # exit entrypoint.sh as soon as something has an exit code other than 0 (error)
 set -e
 
-echo "what's up?"
+CONFIG="/config/settings.json"
+
+# JSON lesen (Node ist vorhanden)
+json() {
+  node -p "(() => {try {
+    const j = require('fs').readFileSync('$CONFIG', 'utf8');
+    return JSON.parse(j)['$1'] ?? '';
+  } catch { return ''; }})()"
+}
+
+# Settings (falls vorhanden)
+if [ -f "$CONFIG" ]; then
+  export GIT_URL="$(json url)"
+  export GIT_BRANCH="$(json branch || echo master)"
+  export DEST="$(json dest || echo /destination)"
+
+  git clone --depth=1 --branch $GIT_BRANCH $GIT_URL src
+else
+  echo "⚠️   no settings.json found"
+fi
 
 exec "$@"
